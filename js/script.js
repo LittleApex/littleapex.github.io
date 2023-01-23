87c1010420632712ecead7a616cb7f2a1dc98c9b
Anastasia <79627149+anasfreud@users.noreply.github.com>
2023-01-23 07:06:39 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ import menu from './modules/menu.js';
 import carousel from './modules/carousel.js';
 import partners from './modules/partners.js';
 import FAQ from './modules/FAQ.js';
-import formService from './modules/formService.js';
+import scroll from './modules/scroll.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   menu();
   carousel();
   partners();
   FAQ();
-  formService();
+  scroll();
 });
 
